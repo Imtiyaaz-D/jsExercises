90df9f9125064f8af1d4a050248387db254b3e48
Imtiyaaz-D <imtdamon777@gmail.com>
2023-05-24 15:34:31 +0200

    question 1 done

---
+++ script/code.js
@@ -0,0 +1,48 @@
+// 1. Variable
+// a value that can change, depending on conditions or on information passed to the program.
+
+// 2. Value
+// the representation of some entity that can be manipulated by a program
+
+// 3. Data type
+// a particular kind of data item, as defined by the values it can take, the programming language used, or the operations that can be performed on it.
+
+// 4. Initialization
+//  to set (something, such as a computer program counter) to a starting position, value, or configuration
+
+// 5. Tell me the difference between let, var, and const.
+ /* 
+var:
+-can be declared outside any function to be used inside any function
+-can be declared inside any function or any other {} which are of only if or if-else or switch etc. and can be used anywhere inside the function
+-can be changed again and again anywhere
+let:
+-can be declared outside any function to be used inside any function
+-if declared inside any function or any other {} which are of only if or if-else or switch etc. and can't be used anywhere inside the function and can be only used inside statement
+- can be changed again and again only inside the statement in which they are made in
+const:
+-can be declared outside any function to be used inside any function
+-can be declared inside any function or any other {} which are of only if or if-else or switch etc. and can be used anywhere inside the function
+-cannot be changed again and agan anywhere, if tried to, will result in an error
+
+*/
+// 6. What is a variable scope?
+// A scope is a region of the program and broadly speaking there are three places, where variables can be declared:
+// Inside a function or a block which is called local variables,
+// In the definition of function parameters which is called formal parameters.
+// Outside of all functions which is called global variables.
+
+// 7. When can you decide when to use a for or while, forEach and do while loop?
+// for − loops through a block of code a specified number of times.
+//  while − loops through a block of code if and as long as a specified condition is true. 
+//  do... while − loops through a block of code once, and then repeats the loop as long as a special condition is true.
+
+// 8. Define hoisting and variable scoping?
+// Hoisting is the process of moving function declarations to the top of the file.
+// variable scoping is a place where variables can be declared for example,inside a function , in the defnition of formal parameters.
+let fullName = 'Imtiyaaz Damon'
+function display(fullName){
+    console.log(fullName)
+}
+
+display(fullName)
