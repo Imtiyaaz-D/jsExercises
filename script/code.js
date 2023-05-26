1e9e7c892021dac310cefaa46c98b778e554aa7e
Imtiyaaz-D <imtdamon777@gmail.com>
2023-05-26 11:08:17 +0200

    question 3 done

--- script/code.js
+++ script/code.js
@@ -54,3 +54,52 @@ let age = '20'
 let subjects = ['Math','English','Afrikaans','Physics','Life orentation','Design']
 let address = ['Swart Street','14','Athlone','South Africa']
 console.log(firstName,lastName,age,subjects,address)
+ 
+// Question 3
+
+function calculate() {
+    let number1 = parseInt(document.getElementById("number1").value);
+    let number2 = parseInt(document.getElementById("number2").value);
+    let operator = document.getElementById("operator").value;
+
+    // create a logical calculation based on the selected operator
+
+    let result;
+
+    // if (operator) {
+        
+    // }
+
+    // will use switch - case for our logic with breaks if an operator has been selected. I t wil also raise an Error if the operator chosen isnt one of the ones already provided.
+
+    switch (operator) {
+        case "+":
+            result = number1 + number2;
+            break;
+        case "-":
+            result = number1 - number2;
+            break;
+        case "*":
+            result = number1 * number2;
+            break;
+        case "**":
+            result = number1**number2;
+            break;
+        case "/":
+            result = number1 / number2
+            break;
+        default:
+            result = "Invalid operator chosen, Please select the available operators";
+    }
+
+    document.getElementById("result").textContent = "Result: " + result;
+}
+
+// We created a button to clear the inputed values and select a new equation more efficiently.
+
+function clearForm() {
+    document.getElementById("number1").value = "";
+    document.getElementById("number2").value = "";
+    document.getElementById("operator").selectedIndex = 0;
+    document.getElementById("result").textContent = "";
+}
